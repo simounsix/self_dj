a027f7a2d4edce509bed637598a07d50e40ac541
simounsix <159032886+simounsix@users.noreply.github.com>
2025-10-03 11:24:34 +0200

    COMMIT

--- script.js
+++ script.js
@@ -92,3 +92,13 @@ document.addEventListener('DOMContentLoaded', () => {
   showSlide(0);
   startAuto();
 });
+
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add('show');
+  } else {
+    backToTop.classList.remove('show');
+  }
+});
